402d78422b283d82ef7443a25cc04c3fa0d26520
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-10-05 00:47:37 +0800

    javascript intro

---
+++ Udemy-Web_Dev_by_Angela_YU/JavaScript/lifesInWeeks.js
@@ -0,0 +1,9 @@
+// remaining before you reach 90 of age
+function lifesInWeeks(age){
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+}
+lifesInWeeks(23);
